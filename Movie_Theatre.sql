ced20d63b8556e0ddd2dcc21307ffb408774e0e6
Dyanna <dyanna.person@gmail.com>
2022-10-19 21:10:10 -0400

    movie

--- Movie_Theatre.sql
+++ Movie_Theatre.sql
@@ -17,7 +17,8 @@ CREATE TABLE ticket(
 	price NUMERIC(5,2) NOT NULL,
 	show_id VARCHAR(100) NOT NULL,
 	show_date VARCHAR(50) NOT NULL,
-	tickets_sold VARCHAR(100),
+	tickets_sold INTEGER(10),
+	movie_id INTEGER(10), 
 	FOREIGN KEY(movie_id) REFERENCES movie(movie_id)
 
 );
@@ -30,3 +31,17 @@ CREATE TABLE ticket(
 -- I am not sure whats going on. I am not even changing anything in the movie TABLE 
 -- yet i keep getting erros that the column movie_id does not exist due to me chnaging the variable type
 -- in another table
+
+-- Eventually was able to get the table created after adding the movie_id variable before the Foreign Key
+
+CREATE TABLE customer(
+	customer_id SERIAL PRIMARY KEY,
+	first_name VARCHAR(10) NOT NULL,
+	phone_num VARCHAR(15) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR(100),
+	vip_member BOOLEAN, 
+	ticket_id INTEGER,
+	FOREIGN KEY(ticket_id) REFERENCES ticket(ticket_id)
+
+);
